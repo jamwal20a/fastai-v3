64f25d1ab2977e527f76124bdb41fa7f91dc2d1b
discdiver <jeffmshale@gmail.com>
2019-03-21 18:33:13 -0400

    update files to serve and requirements

--- app/server.py
+++ app/server.py
@@ -8,10 +8,12 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-1YsNb182_z3NeFSWJK1CMn8jEuZUtXD'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Melanocytic nevi', 'Melanoma', 
+    'Benign keratosis', 'Basal cell carcinoma',  
+    'Actinic keratoses', 'Vascular lesions', 'Dermatofibroma' ]
 path = Path(__file__).parent
 
 app = Starlette()
